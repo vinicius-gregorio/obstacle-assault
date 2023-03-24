41061499a0fc6c916d9dbfef9b27d70aa4f31800
Vinicius Gregorio <vincamgreg@hotmail.com>
2023-03-24 12:20:07 -0300

    feat: create vsCode project

---
+++ README.md
@@ -0,0 +1,3 @@
+# ObstacleAssault2
+
+Developed with Unreal Engine 5
---
+++ Source/ObstacleAssault2/ObstacleAssault2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ObstacleAssault2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ObstacleAssault2, "ObstacleAssault2" );
